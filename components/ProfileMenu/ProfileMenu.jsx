7d0a4bec9b846e82f60807d6d740f68d9c0b5d33
paulius-sak <aspauliusas@gmail.com>
2024-07-02 19:55:27 +0300

    profile menu updated

--- components/ProfileMenu/ProfileMenu.jsx
+++ components/ProfileMenu/ProfileMenu.jsx
@@ -1,28 +1,30 @@
-import React from 'react'
-import styles from "./ProfileMenu.module.css"
-import Link from 'next/link'
+import React from "react";
+import styles from "./ProfileMenu.module.css";
+import Link from "next/link";
 import logoutSvg from "../../assets/icons/logout.svg";
+import accountSmallSvg from "../../assets/icons/accountSmall.svg"
 
-const ProfileMenu = ({logout, user}) => {
+const ProfileMenu = ({ logout, user }) => {
   return (
     <section className={styles.wrapper}>
-        <img className={styles.avatar} src={user.avatarUrl} alt="" />
+      <img className={styles.avatar} src={user.avatarUrl} alt="" />
+      <section className={styles.accountInfo}>
         <h4>{user.name}</h4>
         <h4>{user.email}</h4>
-        <h4>Joined: {user.date.split("T")[0]}</h4>
+        <small>Joined: {user.date.split("T")[0]}</small>
+      </section>
+      <div className={styles.line}></div>
 
-        <div className={styles.line}></div>
+      <section className={styles.menu}>
+        <Link className={styles.profile} href="/account"><img src={accountSmallSvg.src} alt="profile" /> Profile</Link>
 
-        <Link href="/account">👤 Profile</Link>
-        <div className={`${styles.logout} ${styles.tooltipContainer}`}>
-                <button onClick={logout}>
-                  <img src={logoutSvg.src} alt="logout" />
-                  Logout
-                </button>
-                
-              </div>
+        <button className={styles.logoutBtn} onClick={logout}>
+          <img src={logoutSvg.src} alt="logout" />
+          <div>Logout</div>
+        </button>
+      </section>
     </section>
-  )
-}
+  );
+};
 
-export default ProfileMenu
+export default ProfileMenu;
