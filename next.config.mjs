c81ebd8b703d41a68b19ab792fd75e2971eda489
paulius-sak <aspauliusas@gmail.com>
2024-08-04 17:32:57 +0300

    sd

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    SERVER_URL: "http://localhost:3003",
-    // SERVER_URL: "https://forum-node-js.onrender.com",
+    // SERVER_URL: "http://localhost:3003",
+    SERVER_URL: "https://forum-node-js.onrender.com",
   },
 };
 
